18d9f4c83ef1758b9d55c2f89a4fa4dc0060ea98
msaracevic <msaracevic@users.noreply.github.com>
2018-06-13 20:47:45 +0200

    Refactor: Rewrite warframe, remove some legacy code

--- frontend/src/components/Warframe/index.js
+++ frontend/src/components/Warframe/index.js
@@ -7,24 +7,34 @@ import Sortie from './Sortie';
 import Invasions from './Invasions';
 import Fissures from './Fissures';
 import Cetus from './Cetus';
-import Resources from './Resources';
+import Planets from './Planets';
 
-export default class WarframeStatus extends Component {
+const cetusTabHeader = (cetus) => {
+  let label;
+  cetus.day ? label = 'Day' : label = 'Night';
+  if (cetus.timeDayNightRemaining.minutes < 5 && cetus.timeDayNightRemaining.hours === 0) {
+    label += ` (${cetus.timeDayNightRemaining.minutes} min)`
+  }
+  return `Cetus - ${label}`;
+};
+
+const countableTabHeader = (label, object) => {
+  let keys = Object.keys(object);
+  return `${label} (${keys.map(key => object[key].length).reduce((total, currentLength) => total + currentLength)})`;
+};
+
+export default class Warframe extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      status: {}
-    };
+    this.state = {};
   };
 
   getData() {
-    fetch('/api/get-warframe-status')
+    fetch('/api/warframe')
       .then(response => response.json())
       .then(response => {
-        this.setState({
-          status: response
-        });
+        this.setState({...response});
         console.log(response);
       });
   }
@@ -35,31 +45,29 @@ export default class WarframeStatus extends Component {
   }
 
   render() {
-    if (Object.keys(this.state.status).length === 0) {
+    if (Object.keys(this.state).length === 0) {
       return <LoadingScreen/>;
     } else {
-      let alertCount = this.state.status.alerts.length,
-          cetusDay   = this.state.status.cetus.day ? 'Day' : 'Night';
       return (
         <article>
           <Tabs tabs={[{
-            title:   `Cetus (${cetusDay})`,
-            content: <Cetus cetus={this.state.status.cetus}/>
+            title:   `Alerts (${this.state.alerts.length})`,
+            content: <Alerts alerts={this.state.alerts}/>
           }, {
-            title:   `Alerts (${alertCount})`,
-            content: <Alerts alerts={this.state.status.alerts}/>
+            title:   countableTabHeader('Fissures', this.state.fissures),
+            content: <Fissures fissures={this.state.fissures}/>
           }, {
-            title:   `Fissures`,
-            content: <Fissures fissures={this.state.status.fissures}/>
+            title:   countableTabHeader('Invasions', this.state.invasions),
+            content: <Invasions invasions={this.state.invasions}/>
+          }, {
+            title:   cetusTabHeader(this.state.cetus),
+            content: <Cetus cetus={this.state.cetus}/>
           }, {
             title:   `Sortie`,
-            content: <Sortie sortie={this.state.status.sortie}/>
+            content: <Sortie sortie={this.state.sortie}/>
           }, {
-            title:   `Invasions`,
-            content: <Invasions invasions={this.state.status.invasions}/>
-          }, {
-            title:   `Resources`,
-            content: <Resources planets={this.state.status.planets}/>
+            title:   `Planets`,
+            content: <Planets planets={this.state.planets}/>
           }
           ]}/>
         </article>
